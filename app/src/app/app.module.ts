8281f1139a067eba5f42401caa4dd2b9ea724e9f
Mason Frey <mason.frey@gmail.com>
2022-02-19 15:20:59 -0500

    Added Material design components for Angular

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SortComponent } from './sort/sort.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { SortComponent } from './sort/sort.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
