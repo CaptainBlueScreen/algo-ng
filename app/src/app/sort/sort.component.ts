8dbd755009721dbe43dcd1d821986b0ff9af0942
Mason Frey <mason.frey@gmail.com>
2022-02-27 22:49:05 -0700

    Cleaned up unused references and some comments

--- app/src/app/sort/sort.component.ts
+++ app/src/app/sort/sort.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit, ElementRef, NgZone, Renderer2, OnDestroy} from '@angular/core';
-import { fromEvent, Observable, Subscription } from "rxjs";
+import { Component, OnInit, ElementRef, NgZone, OnDestroy} from '@angular/core';
 import { ViewportRuler } from '@angular/cdk/scrolling';
-
-
 import * as p5 from 'p5';
 
 @Component({
@@ -12,30 +9,19 @@ import * as p5 from 'p5';
 })
 export class SortComponent implements OnInit, OnDestroy {
 
-  // resizeObservable$: Observable<Event>
-  // resizeSubscription$: Subscription
-
   private canvas: p5;
-  private width: number;
-  private height: number;
   private readonly viewportChange = this.viewportRuler
     .change(200)
     .subscribe(() => this.ngZone.run(() => this.setSize()));
 
   constructor(
     private el: ElementRef,
-    private renderer: Renderer2,
     private readonly viewportRuler: ViewportRuler,
     private readonly ngZone: NgZone
   ) {
   }
 
   ngOnInit() {
-
-    // this.resizeObservable$ = fromEvent(window, 'resize')
-    // this.resizeSubscription$ = this.resizeObservable$.subscribe( evt => {
-    //   console.log('event: ', evt)
-    // })
     console.log("OnInit");
     this.canvas = new p5(p => {
       let x = 200;
@@ -57,14 +43,11 @@ export class SortComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    // this.resizeSubscription$.unsubscribe()
     this.viewportChange.unsubscribe();
   }
 
   private setSize() {
     const { width, height } = this.viewportRuler.getViewportSize();
-    this.width = width;
-    this.height = height;
     this.canvas.resizeCanvas(width - 10, height - 10);
   }
 }
